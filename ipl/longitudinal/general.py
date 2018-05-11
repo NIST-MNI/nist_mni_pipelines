49eba1038fc94f8c544080ebd2252868bb4b112f
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2018-05-11 10:58:48 -0400

    Fixing strings handling for python3

--- ipl/longitudinal/general.py
+++ ipl/longitudinal/general.py
@@ -16,6 +16,23 @@ from shutil import rmtree
 import tempfile
 
 
+# hack to make it work on Python 3
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
+
 class IplError(Exception):
     def __init__(self, value=''):
         self.value = value
