f1dbb663beca5fc902d29581657ab4d6ce0d9ff8
rphellan <rphellan2210@gmail.com>
2020-12-21 10:09:04 -0500

    Adding ventricle segmentation.

--- ipl/lp/pipeline.py
+++ ipl/lp/pipeline.py
@@ -1039,8 +1039,8 @@ def standard_pipeline(info,
                                 #start with t1w_tal_noscale and then segment ventricle
                                 tmp_work = minc.tmp('tmp_work')
                                 tmp_output = minc.tmp('tmp_output')
-                                fusion_library_description = SegLibrary(options['fusion_library_ve_description'])
-                                fusion_parameters = json.load(open(options['fusion_ve_parameters']))
+                                fusion_library_ve_description = SegLibrary(options['fusion_library_ve_description'])
+                                fusion_ve_parameters = json.load(open(options['fusion_ve_parameters']))
                                 fusion_segment(input_scan= t1w_tal_noscale.scan,
                                             library_description=fusion_library_ve_description,
                                             output_segment=tmp_output,
