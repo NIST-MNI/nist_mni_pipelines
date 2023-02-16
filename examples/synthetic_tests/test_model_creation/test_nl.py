99443afb47fe8595daf78a439001242b86de3dc2
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2023-02-16 10:11:42 -0500

    Refactoring tests for ray

--- examples/synthetic_tests/test_model_creation/test_nl.py
+++ examples/synthetic_tests/test_model_creation/test_nl.py
@@ -1,20 +1,21 @@
 import ray
+import os
 
-from ipl.model.generate_nonlinear  import generate_nonlinear_model_csv
+from ipl.model_ants.generate_nonlinear  import generate_nonlinear_model_csv
 
 if __name__ == '__main__':
-  ray.init(num_cpus=4)
   # setup data for parallel processing
+  os.environ['ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS']='1'
+  ray.init(num_cpus=9)
+
   generate_nonlinear_model_csv('subjects.lst',
-    work_prefix='nl_model',
-    options={'symmetric':False,
+    work_prefix='tmp_nl_sym',
+    options={'symmetric':True,
              'protocol': [{'iter':4,'level':16},
-                          {'iter':4,'level':8},
-                          ],
-             'cleanup': True
+                          {'iter':4,'level':8}, ],
+            'cleanup':True
             },
     model='test_data/ellipse_1.mnc',
-    mask='test_data/mask.mnc',
-    #stop_early=4,
-    #skip=0,
+    mask='test_data/mask.mnc'
+         
   )
