449ba2b53fa54c6277254d4da0634a80b654bd7a
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2024-04-09 10:44:39 -0400

    Modernizing processing scripts

--- container/run_pipeline.sh
+++ container/run_pipeline.sh
@@ -38,6 +38,7 @@ function Usage {
   --- Input data, choose one ---
     -csv <input.csv>               :  input csv file with columns: subject,visit,t1w[t2w,pdw,flair], with header
     -l <input.csv>                 :  input list file with columns: subject,visit,t1w[t2w,pdw,flair], no header
+    -json <input.json>             :  input list file  in json format with columns: subject,visit,t1w[t2w,pdw,flair]
 
   --- output data ---
     -out <output dir>              :  output directory
@@ -48,6 +49,7 @@ function Usage {
     -threads <int>                  :  number of maximu threads per process (default 4), should be less then number of processes
     -field <string>                 :  1.5 or 3 (default 3) scanner field strength
     -cleanup                        :  remove intermediate files to save disk space
+    -fast                           :  run fast version of the pipeline, mostly for testing (rough nonlinear registration, no denoising)
 EOF
 }
 if [[ $# -eq 0 ]]; then Usage; exit 1; fi
@@ -56,12 +58,14 @@ while  [[ $# -gt 0 ]]; do
   if   [[ $1 = -help ]]; then Usage; exit 1
   elif [[ $1 = -u ]]; then Usage; exit 1
   elif [[ $1 = -csv ]]; then in_par="--csv $2"; shift 2
+  elif [[ $1 = -json ]]; then in_par="--json $2"; shift 2
   elif [[ $1 = -l ]]; then in_par="-l $2"; shift 2
   elif [[ $1 = -out    ]]; then out_pfx=$2;shift 2;
   elif [[ $1 = -prl    ]]; then PRL=$2;shift 2;
   elif [[ $1 = -threads    ]]; then THREADS=$2;shift 2;
   elif [[ $1 = -field    ]]; then FIELD=$2;shift 2;
   elif [[ $1 = -cleanup ]]; then CLEANUP=YES; shift
+  elif [[ $1 = -fast ]]; then FAST=YES; shift
   else
     args=( ${args[@]} $1 )
     shift
@@ -84,6 +88,7 @@ PRL=${PRL:-4}
 THREADS=${THREADS:-4}
 FIELD=${FIELD:-3}
 CLEANUP=${CLEANUP:-NO}
+FAST=${FAST:-NO}
 
 ########
 
@@ -93,21 +98,25 @@ else
     CLEANUP=
 fi
 
+if [[ $FAST == YES ]];then
+    fast_par="--nl_step 4.0 "
+else
+    fast_par="--denoise  --nl_step 1.0 "
+fi
+
 if [[ $FIELD == 3 ]];then
     echo "Running 3T pipeline"
-
 #        --wmh_bison_atlas_pfx /opt/models/wmh_bison_1.3.0/3T_2009c_ \
-    python /opt/pipeline/iplLongitudinalPipeline.py \
+    python /opt/pipeline/ipl_longitudinal_pipeline.py \
         -3 \
-        --denoise \
-        --csv $CSV \
+        $fast_par \
+        $in_par \
         -o $out_pfx \
         --model-dir=/opt/models/icbm152_model_09c \
         --model-name=mni_icbm152_t1_tal_nlin_sym_09c  \
         --ray_start $PRL \
         --threads $THREADS \
         --nl_ants \
-        --nl_step 1.0 \
         --nl_cost_fun CC \
         --bison_pfx /opt/models/ipl_bison_1.3.0 \
         --bison_method  HGB1 \
@@ -120,16 +129,15 @@ else
 
 #        --wmh_bison_atlas_pfx /opt/models/wmh_bison_1.3.0/15T_2009c_ \
 
-    python /opt/pipeline/iplLongitudinalPipeline.py \
-        --denoise \
-        -l $in_lst \
+    python /opt/pipeline/ipl_longitudinal_pipeline.py \
+        $fast_par \
+        $in_par \
         -o $out_pfx \
         --model-dir=/opt/models/icbm152_model_09c \
         --model-name=mni_icbm152_t1_tal_nlin_sym_09c  \
         --ray_start $PRL \
         --threads $THREADS \
         --nl_ants \
-        --nl_step 1.0 \
         --nl_cost_fun CC \
         --bison_pfx /opt/models/ipl_bison_1.3.0 \
         --bison_method  HGB1 \
