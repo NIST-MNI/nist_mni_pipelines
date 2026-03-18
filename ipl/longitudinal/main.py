50a3bb04444993b9cfbc4da77b7f731717da0e42
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2026-03-18 10:47:29 -0400

    Removed obsolete debug message

--- ipl/longitudinal/main.py
+++ ipl/longitudinal/main.py
@@ -301,7 +301,6 @@ def runPipeline(pickle=None, patient=None, workdir=None):
         # prepare qc folder
         tps=sorted(list(patient.keys()))
 
-        print(f"{patient.redskull_onnx=} {patient.redskull_native=}")
         # first stage A, multithreading steps
         ray.get([runTimePoint_FirstStageA.remote(tp, patient) for tp in tps])
         patient.write(patient.pickle)  # copy new images in the pickle
